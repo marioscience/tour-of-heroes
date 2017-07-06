95cc858c3284405ef7073ed632386dc0a0759076
marioscience <marioscience@hotmail.com>
2017-07-05 22:14:48 -0400

    Advanced to routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from "./dashboard.component";
+import { HeroesComponent } from "./heroes.component";
+import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroService } from "./hero.service";
+
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
